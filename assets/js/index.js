8b49585d494b26b7a6da34f930861ad44587a482
Vlad Korochanksiy <mginster234942@gmail.com>
2022-07-02 11:12:23 +0300

    fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,32 @@
+/*First method*/
+
 const buttons = document.querySelectorAll(".buttons .button");
 const wrapperElement = document.querySelector(".wrapper-element");
 
-const wrapperItem = document.querySelectorAll(".wrapper-element .wrapper-item");
+// for (const button of buttons) {
+//   button.addEventListener("click", btnHadler);
+// }
+// function btnHadler({ target }) {
+//   for (const button of buttons) {
+//     button.style.backgroundColor = "initial";
+//     button.style.color = "initial";
+//   }
+//   target.style.backgroundColor = " var(--main-color)";
+//   target.style.color = " white";
+
+//   wrapperElement.style.flexDirection = target.dataset.flexDirection;
+// }
+
+/*Second method*/
 
 for (const button of buttons) {
   button.addEventListener("click", btnHadler);
 }
+
 function btnHadler({ target }) {
-  if (
-    target.dataset.style === "column" ||
-    target.dataset.style === "column-reverse"
-  ) {
-    for (const item of wrapperItem) {
-      item.style.width = "100%";
-    }
-  } else if (
-    target.dataset.style === "row" ||
-    target.dataset.style === "row-reverse"
-  ) {
-    for (const item of wrapperItem) {
-      item.style.width = "50px";
-    }
+  for (const button of buttons) {
+    button.classList.remove("active");
   }
-  wrapperElement.style.flexDirection = target.dataset.style;
+  target.classList.add("active");
+  wrapperElement.style.flexDirection = target.dataset.flexDirection;
 }
